97d979a9e220a03326e6ff79012b6d456cb05bc4
Mansur Biryukov <ele638@gmail.com>
2018-12-15 21:53:30 +0300

    Presenter add

--- app/src/main/java/com/example/user/jscanner/presenters/BasePresenter.java
+++ app/src/main/java/com/example/user/jscanner/presenters/BasePresenter.java
@@ -2,9 +2,12 @@ package com.example.user.jscanner.presenters;
 
 import com.example.user.jscanner.activities.DetailActivity;
 
+import java.util.logging.Handler;
+
 public class BasePresenter {
 
     private DetailActivity activity;
+    private Handler handler;
 
     public void onAttach(DetailActivity activity) {
         this.activity = activity;
@@ -13,4 +16,27 @@ public class BasePresenter {
     public void onDetach(){
         this.activity = null;
     }
+
+    public void process(String code){
+        activity.showPB();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            activity.hidePB();
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+    }
+
 }
