2329079cc287cccf237f2c13a5d9f4d6c58ae70f
Alexey Malakhov <au.malakhov@gmail.com>
2018-12-15 20:54:29 +0300

    got test responses from api

--- app/src/main/java/com/example/user/jscanner/activities/DetailActivity.java
+++ app/src/main/java/com/example/user/jscanner/activities/DetailActivity.java
@@ -3,14 +3,46 @@ package com.example.user.jscanner.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.example.user.jscanner.R;
+import com.example.user.jscanner.model.Country;
+import com.example.user.jscanner.network.RestApi;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class DetailActivity extends AppCompatActivity {
+    public static final String SOURCE_URL = "http://daracul.000webhostapp.com/countries.csv";
+    private static final String LOG_TAG = "Testlog" ;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
+
+        Disposable disposable = RestApi.getInstance().countriesEndpoint().countryObject(SOURCE_URL).
+                subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new io.reactivex.functions.Consumer<List<Country>>() {
+                    @Override
+                    public void accept(List<Country> countries) throws Exception {
+                        for (Country country: countries){
+                            Log.d(LOG_TAG, country.getStartWtih()+" " +country.getCountry());
+                        }
+
+                    }
+                }, new io.reactivex.functions.Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Log.d(LOG_TAG, throwable.getClass().getSimpleName()+" ");
+                    }
+                });
+
     }
 }
--- app/src/main/java/com/example/user/jscanner/network/CountryEndpoint.java
+++ app/src/main/java/com/example/user/jscanner/network/CountryEndpoint.java
@@ -1,4 +1,16 @@
 package com.example.user.jscanner.network;
 
+import android.support.annotation.NonNull;
+
+import com.example.user.jscanner.model.Country;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface CountryEndpoint {
+    @GET("csv-to-api")
+    Single<List<Country>> countryObject(@Query("source") @NonNull String source);
 }
