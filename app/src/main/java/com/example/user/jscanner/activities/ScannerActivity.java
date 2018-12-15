398d8d5cc5852d23254672b07df14d79cb9a89cf
denis_mironov <d.mironov@rendez-vous.ru>
2018-12-16 00:17:12 +0300

    Create Scanner.

--- app/src/main/java/com/example/user/jscanner/activities/ScannerActivity.java
+++ app/src/main/java/com/example/user/jscanner/activities/ScannerActivity.java
@@ -1,15 +1,100 @@
 package com.example.user.jscanner.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 import com.example.user.jscanner.R;
+import com.example.user.jscanner.presenters.ScannerPresenter;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class ScannerActivity extends AppCompatActivity {
+
+    private ScannerPresenter presenter;
+    private BarcodeDetector barcodeDetector;
+    private SurfaceView surfaceView;
+    private CameraSource cameraSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+
+        surfaceView = findViewById(R.id.surfaceView);
+
+        presenter = new ScannerPresenter();
+        presenter.onAttach(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initBarcodeDetector();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        presenter.onDetach();
+    }
+
+    private void initBarcodeDetector() {
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.EAN_13 | Barcode.EAN_8)
+                .build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(1920, 1080)
+                .setAutoFocusEnabled(true)
+                .build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(ScannerActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        cameraSource.start(surfaceView.getHolder());
+                    } else {
+                        //ActivityCompat.requestPermissions(ScannerActivity.this, new
+                        //         String[]{Manifest.permission.CAMERA}, );
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+                //Toast.makeText(getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+            }
+        });
     }
 }
